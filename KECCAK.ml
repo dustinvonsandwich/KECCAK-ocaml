63d43d35cffe5a8dca023c9935701c4facd72ce4
dustinvonsandwich <dustiv@uw.edu>
2020-08-21 19:49:16 -0700

    update

--- KECCAK.ml
+++ KECCAK.ml
@@ -5,6 +5,13 @@
 
 
 
-let ROTL64 x y = 
-    (((x) << (y)) | ((x) >> (64 - (y))))
+
+
+
+
+
+(* ROTL64 function as defined by KECCAK specs *)
+let rotl64 x y = 
+    (* (((x) << (y)) | ((x) >> (64 - (y))))*)
+    (x lsl y) lor (x lsr (64 - y))
 
