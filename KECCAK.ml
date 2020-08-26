0de71f0402957e4cba6e686023d26ffa49a02fb3
dustinvonsandwich <dustiv@uw.edu>
2020-08-26 02:47:06 -0700

    update

--- KECCAK.ml
+++ KECCAK.ml
@@ -5,30 +5,14 @@
     ported by Dustin Ray - Summer 2020
 *)
 
+    open Int64
 
     (* ROTL64 function *)
     let rotl64 x y = 
         (* (((x) << (y)) | ((x) >> (64 - (y))))*)
-        (x lsl y) lor (x lsr (64 - y)) 
-
-    (* A note on the difference between the data structure below and a typical imperative structure as we have implemented in
-    Java and Rust: You may see that we are creating a new map every time we add something to KeccakfRndc, and this is in fact what is happening.
-    You may also note that this process would consume a much larger amount of memory compared to an array or a list in an imperative setting, however
-    Ocaml has automatic memory management that allows us to work in this fashion with resource usage comparable to an imperative language.  *)
-    (* keccak[f] round constants as map*)
-    module KeccakfRndc = Map.Make(struct type t = int64 let compare = compare end)
-        let kRndc = KeccakfRndc.empty
-        let kRndc = KeccakfRndc.add 0x0000000000000001L let kRndc = KeccakfRndc.add 0x0000000000008082L let kRndc = KeccakfRndc.add 0x800000000000808aL  
-        let kRndc = KeccakfRndc.add 0x8000000080008000L let kRndc = KeccakfRndc.add 0x000000000000808bL let kRndc = KeccakfRndc.add 0x0000000080000001L  
-        let kRndc = KeccakfRndc.add 0x8000000080008081L let kRndc = KeccakfRndc.add 0x8000000000008009L let kRndc = KeccakfRndc.add 0x000000000000008aL  
-        let kRndc = KeccakfRndc.add 0x0000000000000088L let kRndc = KeccakfRndc.add 0x0000000080008009L let kRndc = KeccakfRndc.add 0x000000008000000aL  
-        let kRndc = KeccakfRndc.add 0x000000008000808bL let kRndc = KeccakfRndc.add 0x800000000000008bL let kRndc = KeccakfRndc.add 0x8000000000008089L  
-        let kRndc = KeccakfRndc.add 0x8000000000008003L let kRndc = KeccakfRndc.add 0x8000000000008002L let kRndc = KeccakfRndc.add 0x8000000000000080L 
-        let kRndc = KeccakfRndc.add 0x000000000000800aL let kRndc = KeccakfRndc.add 0x800000008000000aL let kRndc = KeccakfRndc.add 0x8000000080008081L  
-        let kRndc = KeccakfRndc.add 0x8000000000008080L let kRndc = KeccakfRndc.add 0x0000000080000001L let kRndc = KeccakfRndc.add 0x8000000080008008L
+        (x lsl y) lor (x lsr (64 - y))     
     
-    
-
+(*)
 
     (* keccak[f] rotate constants *)
     module KeccakfRotc = Map.Make(struct type t = int let compare = compare end)
@@ -48,25 +32,45 @@
         let kPiln = KeccakfPiln.add 8 let kPiln = KeccakfPiln.add 21 let kPiln = KeccakfPiln.add 24 let kPiln = KeccakfPiln.add 4 
         let kPiln = KeccakfPiln.add 15 let kPiln = KeccakfPiln.add 23 let kPiln = KeccakfPiln.add 19 let kPiln = KeccakfPiln.add 13 
         let kPiln = KeccakfPiln.add 12 let kPiln = KeccakfPiln.add 2 let kPiln = KeccakfPiln.add 20 let kPiln = KeccakfPiln.add 14 
-        let kPiln = KeccakfPiln.add 22 let kPiln = KeccakfPiln.add 9 let kPiln = KeccakfPiln.add 6 let kPiln = KeccakfPiln.add 1
+        let kPiln = KeccakfPiln.add 22 let kPiln = KeccakfPiln.add 9 let kPiln = KeccakfPiln.add 6 let kPiln = KeccakfPiln.add 1 *)
 
 
-    (*Main permutation. Give me st, Ill give you st back with keccak machinery applied. *)
-    let rec keccakfpermuation = (fun x -> x)
-
-        
-        
 
 
-        let t = 0
 
-        module Bc = Map.Make(struct type t = int64 let compare = compare end)
+(*Arbitrary random data to simulate state input *)
+let st x =             
+    match x with
+    |_ -> 0L
+    |1 -> 0x8596978674635641L |2 -> 0x8493726429093888L |3 -> 0x890938800080895aL  
+    |4 -> 0x8543254256536634L |5 -> 0x435432679857059bL |6 -> 0x05575832495324534L  
+    |7 -> 0x8054325798789481L |8 -> 0x8435463763524559L |9 -> 0x543259965432558aL  
+    |10 -> 0x5432879043127545L |11 -> 0x5543274890321629L |13 -> 0x000000008000000aL  
+    |14 -> 0x00434343214553246bL |15 -> 0x843254365367608bL |16 -> 0x5432542674576429L  
+    |17 -> 0x8653478760987543L |18 -> 0x6534431243219872L |19 -> 0x5432657856653653L 
+    |20 -> 0x657568769543225aL |21 -> 0x854325454325436aL |22 -> 0x8654876865443581L  
+    |23 -> 0x8543275698776540L |24 -> 0x5432645875665421L |25 -> 0x8523745897543208L
 
-        
+let keccakfrndc x = 
 
-
-        
+    match x with
+    |_ -> 0L
+    |1 -> 0x0000000000000001L |2 -> 0x0000000000008082L |3 -> 0x800000000000808aL  
+    |4 -> 0x8000000080008000L |5 -> 0x000000000000808bL |6 -> 0x0000000080000001L  
+    |7 -> 0x8000000080008081L |8 -> 0x8000000000008009L |9 -> 0x000000000000008aL  
+    |10 -> 0x0000000000000088L |11 -> 0x0000000080008009L |13 -> 0x000000008000000aL  
+    |14 -> 0x000000008000808bL |15 -> 0x800000000000008bL |16 -> 0x8000000000008089L  
+    |17 -> 0x8000000000008003L |18 -> 0x8000000000008002L |19 -> 0x8000000000000080L 
+    |20 -> 0x000000000000800aL |21 -> 0x800000008000000aL |22 -> 0x8000000080008081L  
+    |23 -> 0x8000000000008080L |24 -> 0x0000000080000001L |25 -> 0x8000000080008008L
     
 
-        
-        
+
+(* bc[i] = st[i] ^ st[i + 5] ^ st[i + 10] ^ st[i + 15] ^ st[i + 20] *)
+(* y = bc and x = i *)
+let theta1 x y : int64 = 
+    y logxor st (x + 5)
+
+
+
+print_string "works to this line"
